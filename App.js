3718fe266bf0a4d70ae032e3d840a6aab4fe12c5
EC2 Default User <ec2-user@ip-172-31-41-126.eu-west-1.compute.internal>
2020-06-17 13:24:19 +0000

    Second Change

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!Hey Rohit</Text>
     </View>
   );
 }
